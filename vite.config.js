616d55d098bb09fd33d2ed5252ce98f6fe971df6
Hacker God <jyrone.parker@gmail.com>
2025-04-20 10:49:30 +0000

    vite PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,37 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
     plugins: [
+        VitePWA({ 
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+              },
+              manifest: {
+                name: 'Laravel WebRTC',
+                short_name: 'WebRTC',
+                description: 'Simple WebRTC application built with Laravel and Vue.js',
+                theme_color: '#ffffff',
+                icons: [
+                  {
+                    src: 'icon.svg',
+                    sizes: '192x192',
+                    type: 'image/svg+xml'
+                  },
+                  {
+                    src: 'icon.svg',
+                    sizes: '512x512',
+                    type: 'image/svg+xml'
+                  }
+                ]
+              },
+            injectRegister: 'auto',
+            devOptions: {
+                enabled: true
+              }
+         }),
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
